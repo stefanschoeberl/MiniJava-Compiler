0588539c805f6eb45d1949ec2faaad4fc8a063a1
Stefan Schöberl <10100114+stefanschoeberl@users.noreply.github.com>
2019-10-07 18:06:07 +0200

    Change to promises

--- run.js
+++ run.js
@@ -1,16 +1,20 @@
 const fs = require('fs');
+const { promisify } = require("util");
 
 // https://webassembly.org/getting-started/js-api/
 
-function runFile(file) {
-    const imports = {imports: {
-        println: arg => console.log(arg)
-    }};
-    fs.readFile(file, (err, bytes) => {
-        WebAssembly.compile(bytes)
-            .then(m => new WebAssembly.Instance(m, imports))
-            .then(instance => instance.exports.main());
-    });
+const readFile = promisify(fs.readFile);
+
+async function runFile(file) {
+    const imports = {
+        imports: {
+            println: arg => console.log(arg)
+        }
+    };
+    const bytes = await readFile(file);
+    const module = await WebAssembly.compile(bytes);
+    const instance = new WebAssembly.Instance(module, imports);
+    instance.exports.main();
 }
 
 runFile(process.argv[2]);
