ceef13f812bb702a40add4fc45095c90791b8dd0
Stefan Schöberl <10100114+stefanschoeberl@users.noreply.github.com>
2020-04-06 22:48:40 +0200

    Add fallback for browsers not supporting WebAssembly.instantiateStreaming

--- demo-browser/src/index.js
+++ demo-browser/src/index.js
@@ -3,8 +3,16 @@ const imports = require('../build/wasm-module/imports');
 
 async function runModule() {
     const runtime = new Runtime();
-    const instance = await WebAssembly.instantiateStreaming(fetch('module.wasm'), imports(runtime));
-    instance.instance.exports["Main.main"]();
+    if (typeof WebAssembly.instantiateStreaming === 'function') {
+        const instance = await WebAssembly.instantiateStreaming(fetch('module.wasm'), imports(runtime));
+        instance.instance.exports["Main.main"]();
+    } else {
+        const response = await fetch('module.wasm');
+        const arrayBuffer = await response.arrayBuffer();
+        const module = await WebAssembly.compile(arrayBuffer);
+        const instance = await WebAssembly.instantiate(module, imports(runtime));
+        instance.exports["Main.main"]();
+    }
 }
 
 runModule();
