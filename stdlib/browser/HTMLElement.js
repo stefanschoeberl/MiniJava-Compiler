cf8f5be33b15217f72f6ecd3eb6a08a960acc066
Stefan Schöberl <10100114+stefanschoeberl@users.noreply.github.com>
2020-04-07 20:42:17 +0200

    Finish browser demo application

--- stdlib/browser/HTMLElement.js
+++ stdlib/browser/HTMLElement.js
@@ -3,5 +3,12 @@ module.exports = runtime => {
         "HTMLElement.appendChild#HTMLElement": (thisRef, newChildRef) => {
             runtime.wasmDeref(thisRef).appendChild(runtime.wasmDeref(newChildRef));
         },
+        "HTMLElement.addClickEventListener#Object": (thisRef, handlerRef) => {
+            const element = runtime.wasmDeref(thisRef);
+            const handler = runtime.wasmDeref(handlerRef);
+            element.addEventListener('click', (event) => {
+                runtime.instanceMethod(handler, 'handleEvent', 'MouseEvent')(event);
+            })
+        }
     }
 };
