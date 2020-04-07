bdcf15dd4f14763d3bf64b75dc18fcbda89fedc0
Stefan Schöberl <10100114+stefanschoeberl@users.noreply.github.com>
2020-04-07 19:56:43 +0200

    Rename minijava files to UpperCamelCase style

--- stdlib/browser/Document.js
+++ stdlib/browser/Document.js
@@ -2,11 +2,11 @@ module.exports = runtime => {
     return {
         "Document.getElementById#String": elementIdRef => {
             const elementId = runtime.wasmDeref(elementIdRef);
-            return runtime.wasmRef(document.getElementById(elementId));
+            return runtime.wasmRef(document.getElementById(elementId), "HTMLElement");
         },
         "Document.createElement#String": tagNameRef => {
             const tagName = runtime.wasmDeref(tagNameRef);
-            return runtime.wasmRef(document.createElement(tagName));
+            return runtime.wasmRef(document.createElement(tagName), "HTMLElement");
         }
     }
 };
