26122bd2740a536e9fd6d02675a23e5e7c0602e7
Stefan Schöberl <10100114+stefanschoeberl@users.noreply.github.com>
2020-04-07 20:49:50 +0200

    Refactor wasm-version.js

--- demo-browser/src/wasm-version.js
+++ demo-browser/src/wasm-version.js
@@ -3,18 +3,20 @@ const imports = require('../build/wasm-module/imports');
 
 async function runModule() {
     const runtime = new Runtime();
+    let instance;
     if (typeof WebAssembly.instantiateStreaming === 'function') {
-        const instance = await WebAssembly.instantiateStreaming(fetch('module.wasm'), imports(runtime));
-        instance.instance.exports["Main.main"]();
+        const result = await WebAssembly.instantiateStreaming(fetch('module.wasm'), imports(runtime));
+        instance = result.instance;
+
     } else {
         const response = await fetch('module.wasm');
         const arrayBuffer = await response.arrayBuffer();
         const module = await WebAssembly.compile(arrayBuffer);
-        const instance = await WebAssembly.instantiate(module, imports(runtime));
-
-        runtime.setWasmModuleInstance(instance);
-        runtime.staticMethod("Main", "main")();
+        instance = await WebAssembly.instantiate(module, imports(runtime));
     }
+
+    runtime.setWasmModuleInstance(instance);
+    runtime.staticMethod("Main", "main")();
 }
 
 module.exports = runModule;
